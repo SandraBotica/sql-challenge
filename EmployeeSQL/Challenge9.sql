22ab6f9000ea99b95257443b8fa33b91439196a4
SandraBotica <smbotica@gmail.com>
2023-02-12 19:37:48 +0800

    questions 1-8

--- EmployeeSQL/Challenge9.sql
+++ EmployeeSQL/Challenge9.sql
@@ -77,12 +77,68 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+--Question 1
 select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from employees e
 left join salaries as s
 on e.emp_no = s.emp_no;
 
-
+--Question 2
 select e.first_name, e.last_name
 from employees as e
 where year(hire_date) = "%/%/1986";
+
+--Question 3
+
+select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name  
+from departments as d
+inner join dept_manager as dm on
+dm.dept_no=d.dept_no
+inner join employees as e on
+e.emp_no = dm.emp_no ;
+
+--Question 4
+
+select d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+from departments as d
+inner join dept_emp as de on
+de.dept_no=d.dept_no
+inner join employees as e on
+e.emp_no = de.emp_no ;
+
+--Question 5
+
+select e.first_name, e.last_name, e.sex
+from employees as e 
+where e.first_name = 'Hercules'
+	and e.last_name like 'B%';
+
+--Question 6
+
+select e.emp_no, e.last_name, e.first_name
+from employees as e
+inner join dept_emp as de on
+de.emp_no = e.emp_no
+inner join departments as d on
+d.dept_no = de.dept_no
+where d.dept_name = 'Sales';
+
+--Question 7
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp as de on
+de.emp_no = e.emp_no
+inner join departments as d on
+d.dept_no = de.dept_no
+where d.dept_name = 'Sales'
+or d.dept_name = 'Development';
+
+--Question 8
+
+select last_name, count(last_name) as "Frequency counts last_name"
+from employees
+group by last_name
+order by "Frequency counts last_name" desc;
+
+
