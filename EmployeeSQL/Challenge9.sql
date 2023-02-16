e7e16f5dc11ac81cb4f8d6ddafdea3e0dc25593e
SandraBotica <smbotica@gmail.com>
2023-02-16 15:38:19 +0800

    completed

--- EmployeeSQL/Challenge9.sql
+++ EmployeeSQL/Challenge9.sql
@@ -1,7 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/f7nw1E
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
 
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
